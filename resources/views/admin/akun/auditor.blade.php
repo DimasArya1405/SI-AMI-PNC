65f9d99693826fa08243a1a24dbbbe1c7caf38ab
DimasArya1405 <165295750+DimasArya1405@users.noreply.github.com>
2026-03-16 20:45:10 +0700

    Modal edit dan hapus

--- resources/views/admin/akun/auditor.blade.php
+++ resources/views/admin/akun/auditor.blade.php
@@ -16,7 +16,7 @@
                     </button>
                 </div>
                 <div class="dt-responsive table-responsive p-4 pt-4">
-                     {!! $dataTable->table(['class' => 'min-w-full text-sm text-left text-gray-700'], true) !!}
+                    {!! $dataTable->table(['class' => 'min-w-full text-sm text-left text-gray-700'], true) !!}
                 </div>
             </div>
         </div>
@@ -111,89 +111,103 @@
     <div id="modal-edit" tabindex="-1" aria-hidden="true"
         class="hidden bg-gray-900/50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] min-h-full">
         <div class="relative p-4 w-full max-w-md max-h-full">
-            <!-- Modal content -->
+
             <div class="relative bg-white border border-default rounded-base shadow-sm p-4 md:p-6">
-                <!-- Modal header -->
+
                 <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
                     <h3 class="text-lg font-medium text-heading">
                         Edit Data Auditor
                     </h3>
+
                     <button type="button"
                         class="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center"
                         data-modal-hide="modal-edit">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
+
+                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                 stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
                         </svg>
-                        <span class="sr-only">Close modal</span>
+
                     </button>
                 </div>
-                <!-- Modal body -->
+
                 <form action="{{ route('admin.auditor.edit') }}" method="post">
                     @csrf
                     @method('put')
+
                     <div class="grid gap-4 grid-cols-2 py-4 md:py-6">
-                        <input type="hidden" name="auditor_id" id="auditor_id"
-                            class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                            required="">
+
+                        <input type="hidden" name="auditor_id" id="auditor_id">
+
                         <div class="col-span-2">
-                            <label for="name" class="block mb-2.5 text-sm font-medium text-heading">Nama</label>
+                            <label class="block mb-2.5 text-sm font-medium text-heading">Nama</label>
                             <input type="text" name="nama" id="nama"
-                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                                required="">
+                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+                                required>
                         </div>
+
                         <div class="col-span-2 sm:col-span-1">
-                            <label for="price" class="block mb-2.5 text-sm font-medium text-heading">NIP</label>
+                            <label class="block mb-2.5 text-sm font-medium text-heading">NIP</label>
                             <input type="number" name="nip" id="nip"
-                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                                required="">
+                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+                                required>
                         </div>
+
                         <div class="col-span-2 sm:col-span-1">
-                            <label for="price"
-                                class="block mb-2.5 text-sm font-medium text-heading">Jabatan</label>
+                            <label class="block mb-2.5 text-sm font-medium text-heading">Jabatan</label>
                             <input type="text" name="jabatan" id="jabatan"
-                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                                required="">
+                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+                                required>
                         </div>
+
                         <div class="col-span-2">
-                            <label for="category" class="block mb-2.5 text-sm font-medium text-heading">Prodi</label>
+                            <label class="block mb-2.5 text-sm font-medium text-heading">Prodi</label>
                             <select id="prodi" name="prodi"
-                                class="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5 shadow-xs placeholder:text-body">
+                                class="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs">
+
                                 <option selected="">Pilih Prodi</option>
+
                                 @foreach ($prodi as $item)
-                                    <option value="{{ $item->prodi_id }}">{{ $item->jenjang }} -
-                                        {{ $item->nama_prodi }}</option>
+                                    <option value="{{ $item->prodi_id }}">
+                                        {{ $item->jenjang }} - {{ $item->nama_prodi }}
+                                    </option>
                                 @endforeach
+
                             </select>
                         </div>
+
                         <div class="col-span-2 sm:col-span-1">
-                            <label for="price" class="block mb-2.5 text-sm font-medium text-heading">Email</label>
+                            <label class="block mb-2.5 text-sm font-medium text-heading">Email</label>
                             <input type="email" name="email" id="email"
-                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                                required="">
+                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+                                required>
                         </div>
+
                         <div class="col-span-2 sm:col-span-1">
-                            <label for="price" class="block mb-2.5 text-sm font-medium text-heading">No. HP</label>
+                            <label class="block mb-2.5 text-sm font-medium text-heading">No. HP</label>
                             <input type="text" name="no_telp" id="no_telp"
-                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                                required="">
+                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs"
+                                required>
                         </div>
+
                     </div>
+
                     <div class="flex items-center space-x-4 border-t border-default pt-4 md:pt-6">
+
                         <button type="submit"
-                            class="inline-flex items-center  text-white bg-blue-500 hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none hover:bg-blue-700 transition duration-200 ease-in-out">
-                            <svg class="w-4 h-4 me-1.5 -ms-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="M5 12h14m-7 7V5" />
-                            </svg>
+                            class="inline-flex items-center text-white bg-blue-500 hover:bg-blue-700 border border-transparent shadow-xs font-medium rounded-base text-sm px-4 py-2.5 transition">
+
                             Simpan
                         </button>
-                        <button data-modal-hide="crud-modal" type="submit"
-                            class="text-body bg-white hover:bg-gray-200 transition duration-300 ease-in-out border border-gray-400 hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Batal</button>
+
+                        <button data-modal-hide="modal-edit" type="button"
+                            class="text-body bg-white hover:bg-gray-200 border border-gray-400 rounded-base text-sm px-4 py-2.5 transition">
+                            Batal
+                        </button>
+
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
@@ -241,6 +255,7 @@
     @push('js')
         <script>
             $(document).on('click', '.button-edit', function() {
+
                 let auditor_id = $(this).data('id');
                 let nip = $(this).data('nip');
                 let nama = $(this).data('nama');
@@ -256,12 +271,27 @@
                 $('#prodi').val(prodi);
                 $('#email').val(email);
                 $('#no_telp').val(no_telp);
+
+                $('#modal-edit').removeClass('hidden').addClass('flex');
+
             });
-        </script>
-        <script>
+
             $(document).on('click', '.button-hapus', function() {
+
                 let auditor_id = $(this).data('id');
+
                 $('#auditor_id_hapus').val(auditor_id);
+
+                $('#modal-hapus').removeClass('hidden').addClass('flex');
+
+            });
+
+            $(document).on('click', '[data-modal-hide="modal-edit"]', function() {
+                $('#modal-edit').removeClass('flex').addClass('hidden');
+            });
+
+            $(document).on('click', '[data-modal-hide="modal-hapus"]', function() {
+                $('#modal-hapus').removeClass('flex').addClass('hidden');
             });
         </script>
     @endpush
